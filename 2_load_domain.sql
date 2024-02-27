b67aef3e473c1cd2e7f56b2dd5fd1ef2056a7e41
williamloosman <william.loosman@kadaster.nl>
2024-02-27 08:45:36 +0100

    Opdracht uitgewerkt

--- 2_load_domain.sql
+++ 2_load_domain.sql
@@ -1,3 +1,3 @@
 -- Zodra je een raw laag hebt in de database kunnen we dmv queries ons domeinmodel opbouwen
 -- Schrijf de queries die we nodig hebben op een domeinmodel op te bouwen
-
+-- Je kunt een table vullen vanuit een query -> zie https://learnsql.com/cookbook/how-to-create-one-table-from-another-table-in-sql/
